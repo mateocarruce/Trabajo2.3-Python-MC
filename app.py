3391bed48fb84796567773f52fa6f8d57306ff50
Mateocarr <mateocarruce@gmail.com>
2024-11-12 01:01:01 -0500

    feat: :memo: mod app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello world from Python, Lesly Salas SI08"
+    return "Distributed programming - Mateo Carrasco"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
