a1758b5fb32ff48b43505ea20e281c2017c02b29
Mateocarr <mateocarruce@gmail.com>
2024-11-12 00:38:47 -0500

    feat: :ambulance: Mod app
    change a port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +8,5 @@ def hello_world():
     return '¡Distributed programming - Mateo Carrasco!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Lee el puerto desde la variable de entorno, o usa 5000 por defecto
+    app.run(host='0.0.0.0', port=port)
